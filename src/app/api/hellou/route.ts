1c587affeb9ea4c285a98e57d9b32dc1b8bfb9a3
martynvalenz <martynvalenz@gmail.com>
2023-11-09 08:44:09 -0600

    first commit

---
+++ src/app/api/hellou/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET(request: Request) { 
+  return NextResponse.json({
+    message: 'Hello from the API'
+  })
+}
+
+export async function POST(request: Request) { 
+  return NextResponse.json({
+    message: 'Hello from the API',
+    method: 'POST'
+  })
+}
