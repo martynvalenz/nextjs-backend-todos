6181c3db282fd73eef42271e25670bda2d7a7e14
martynvalenz <martynvalenz@gmail.com>
2023-11-09 17:14:17 -0600

    Finish CRUD endpoints and yup validations

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,39 @@
+import prisma from '@/lib/prisma';
+import { NextResponse, NextRequest } from 'next/server';
+import * as yup from 'yup';
+
+export async function GET(request: Request) { 
+  const { searchParams } = new URL(request.url);
+  const take = parseInt(searchParams.get('take') ?? '10');
+  const skip = parseInt(searchParams.get('skip') ?? '0');
+  if (isNaN(take)) {
+    return NextResponse.json({ error: 'Invalid take parameter, must be number' }, { status: 400 });
+  }
+  if (isNaN(skip)) {
+    return NextResponse.json({ error: 'Invalid skip parameter, must be number' }, { status: 400 });
+  }
+  const todos = await prisma.todo.findMany({
+    take,
+    skip,
+  });
+  return NextResponse.json(todos);
+}
+
+const postSchema = yup.object({
+  description: yup.string().required(),
+  complete: yup.boolean().optional().default(false), // TODO: add something interesting here
+})
+
+export async function POST(request: Request) {
+  try {
+    const {complete,description} = await postSchema.validate(await request.json());
+    const todo = await prisma.todo.create({
+      data:{complete,description}
+    });
+  
+    return NextResponse.json({todo});
+
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 400 });
+  }
+}
